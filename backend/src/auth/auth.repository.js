143ed565ad1af17251f8bcf9fb91e67dee53b4e5
sadiqfatima382-hue <sadiqfatima382@gmail.com>
2026-08-28 17:10:14 +0500

    Add user and refresh token repository functions for database operations

---
+++ backend/src/auth/auth.repository.js
@@ -0,0 +1,52 @@
+import prisma from "../config/prisma.js";
+
+export async function findUserById(id) {
+    return prisma.user.findUnique({
+        where: { id }
+    })
+}
+
+export async function findUserByEmail(email) {
+    return prisma.user.findUnique({
+        while: { email }
+    })
+}
+
+export async function createUser(data) {
+    return prisma.user.create({
+        data,
+    })
+}
+
+export async function createRefreshToken(data) {
+  return prisma.refreshToken.create({
+    data,
+  });
+}
+
+export async function findRefreshToken(token) {
+  return prisma.refreshToken.findUnique({
+    where: {
+      token,
+    },
+    include: {
+      user: true,
+    },
+  });
+}
+
+export async function deleteRefreshToken(token) {
+  return prisma.refreshToken.delete({
+    where: {
+      token,
+    },
+  });
+}
+
+export async function deleteUserRefreshTokens(userId) {
+  return prisma.refreshToken.deleteMany({
+    where: {
+      userId,
+    },
+  });
+}
