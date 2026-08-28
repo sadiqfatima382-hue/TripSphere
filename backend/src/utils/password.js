3bc3c28e6353602082236b0d80ffec39e1d8c0b1
sadiqfatima382-hue <sadiqfatima382@gmail.com>
2026-08-28 16:07:31 +0500

    Refactor password utility functions to improve readability and maintainability

--- backend/src/utils/password.js
+++ backend/src/utils/password.js
@@ -2,5 +2,11 @@ import bcrypt from "bcrypt";
 import env from "../config/env.js";
 
 export async function hashPassword (password){
+    const salt = await bcrypt.genSalt(Number(env.BCRYPT_SALT_ROUNDS));
+    return bcrypt.hash(password,salt)
+}
+
+export async function comparePasswords(password, hashPassword) {
+    return bcrypt.compare(password, hashPassword)
     
 }
