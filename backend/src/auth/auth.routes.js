f26ea61d948578151d4c0ddab8e69ca3eef8f047
sadiqfatima382-hue <sadiqfatima382@gmail.com>
2026-08-29 15:14:16 +0500

    Add protected route for authenticated user information retrieval

--- backend/src/auth/auth.routes.js
+++ backend/src/auth/auth.routes.js
@@ -1,11 +1,20 @@
-import { Router } from "express";
-
-import { register, login,} from "../auth/auth.controller.js";
-
-const router = Router();
+import express from "express";
+import { register, login, } from "../auth/auth.controller.js";
+import { authenticate } from "../middlewares/auth.middleware.js"
+const router = express.Router();
 
 router.post("/register", register);
-
 router.post("/login", login);
+
+//Protected route
+router.get("/me", authenticate, async (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Authenticated user",
+        data: {
+            user: req.user,
+        },
+    });
+});
 
 export default router;
